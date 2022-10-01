896c203958756a8d9654801ec1d7e131b18539c1
AliMoshtaq <ali.moshtaq@yahoo.com>
2022-10-01 13:17:43 +0200

    added data class and adapter for Home Screen

--- app/src/main/java/co/develhope/meteoapp/MainActivity.kt
+++ app/src/main/java/co/develhope/meteoapp/MainActivity.kt
@@ -2,10 +2,20 @@ package co.develhope.meteoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        replaceFragment(HomeFragment())
+    }
+
+    private fun replaceFragment(homeFragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,homeFragment)
+        fragmentTransaction.commit()
     }
 }
