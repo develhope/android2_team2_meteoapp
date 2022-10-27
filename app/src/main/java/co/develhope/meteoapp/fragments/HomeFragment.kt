dba560102899b90d19803c9217f86e7daa737ac8
AliMoshtaq <ali.moshtaq@yahoo.com>
2022-10-27 22:15:51 +0200

    View repeating issue

---
+++ app/src/main/java/co/develhope/meteoapp/fragments/HomeFragment.kt
@@ -0,0 +1,33 @@
+package co.develhope.meteoapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import co.develhope.meteoapp.adapters.HomeScrAdapter
+import co.develhope.meteoapp.data.ForecastModel
+import co.develhope.meteoapp.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        ForecastModel.getForecastItems()
+        val layoutManager = LinearLayoutManager(context)
+        val recyclerView = binding.homeRecyclerView
+        recyclerView.layoutManager = layoutManager
+        recyclerView.setHasFixedSize(true)
+        val adapter = HomeScrAdapter(ForecastModel.getForecastItems())
+        recyclerView.adapter = adapter
+    }
+}
